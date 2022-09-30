b5e0ef53ceefc2b3b3e3b88b904696a5c5c8ff05
Jonatas Jeremias Pena dos Santos <CIANDT\jonatasps@lnb035693cps.ciandt.global>
2022-09-30 17:41:24 -0300

    envio

--- app/src/main/java/com/jonatas/nybooks/BooksActivity.kt
+++ app/src/main/java/com/jonatas/nybooks/BooksActivity.kt
@@ -2,7 +2,11 @@ package com.jonatas.nybooks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.jonatas.nybooks.data.model.Book
 import com.jonatas.nybooks.databinding.ActivityBooksBinding
+import com.jonatas.nybooks.presentation.BooksAdapter
 import com.jonatas.nybooks.presentation.base.BaseActivity
 
 
@@ -14,8 +18,21 @@ class BooksActivity : AppCompatActivity() {
         val binding = ActivityBooksBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val teste = (binding.toolbarMain( R.string.books_title)
 
+
+        with(binding.recyclerBooks) {
+            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+            adapter = BooksAdapter(getBooks())
+        }
+    }
+
+    fun getBooks(): List<Book> {
+        return listOf<Book>(
+            Book("Title1", "Aythor1"),
+            Book("Title2", "Aythor2"),
+            Book("Title3", "Aythor3")
+        )
     }
 
 
